5202bb71e89816e5551c71022a6cb814113d95e8
Kaj Jansson <mailtokaj@gmail.com>
2022-07-03 17:36:34 +0200

    Cleaned up PNG file name and added texture SVG

--- vvipers/config.hpp
+++ vvipers/config.hpp
@@ -1,3 +1,3 @@
 #define RESOURCE_PATH "/home/kaj/code/vvipers/res/"
-#define VIPER_TEXTURE_FILE "viperhead.png"
+#define VIPER_TEXTURE_FILE "viper_texture.png"
 #define VIPER_FILE_PATH RESOURCE_PATH VIPER_TEXTURE_FILE
