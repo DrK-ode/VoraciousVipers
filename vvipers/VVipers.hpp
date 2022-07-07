c9f3d8bef7866e3d3dc36ae0eb8d0f9a95761d84
Kaj Jansson <mailtokaj@gmail.com>
2022-07-07 11:11:19 +0200

    Documents VVipers class

--- vvipers/VVipers.hpp
+++ vvipers/VVipers.hpp
@@ -6,10 +6,20 @@
 
 namespace VVipers {
 
+/**
+ * Main manager class of the whole game.
+ * This is where the render window is created and the events are polled. The
+ * game loop is framrate limited.
+ */
 class VVipers {
   public:
     VVipers();
     ~VVipers();
+
+    /**
+     * Starts the game loop.
+     * Currently hardcoded to 60 FPS.
+     */
     void startGame();
 
   private:
