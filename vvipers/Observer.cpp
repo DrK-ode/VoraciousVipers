b2be9746f26bc2617401e4f72d991e6b9f4ff10c
Kaj Jansson <mailtokaj@gmail.com>
2022-07-15 20:56:57 +0200

    Observer/Observables work together to clean up when one is deleted.

--- vvipers/Observer.cpp
+++ vvipers/Observer.cpp
@@ -2,19 +2,31 @@
 
 namespace VVipers {
 
+Observable::~Observable(){
+    for( auto observer : m_observers )
+        removeObserver(observer.first);
+}
+
 void Observable::addObserver(Observer* observer,
                              const std::set<GameEvent::EventType>& eventTypes) {
     m_observers[observer] = eventTypes;
+    observer->m_observing.insert(this);
 }
 
 void Observable::removeObserver(Observer* observer) {
     m_observers.erase(observer);
+    observer->m_observing.erase(this);
 }
 
 void Observable::notify(const GameEvent* event) const {
     for (auto& observer : m_observers)
         if (observer.second.contains(event->type()))
             observer.first->onNotify(event);
+}
+
+Observer::~Observer(){
+    for( auto observable : m_observing )
+        observable->removeObserver(this);
 }
 
 }  // namespace VVipers
--- vvipers/Observer.hpp
+++ vvipers/Observer.hpp
@@ -7,18 +7,16 @@
 
 namespace VVipers {
 
-class Observer {
-  public:
-    /** The object event is pointing to is only guaranteed to exist at the time
-     * of notification. If access to the event is needed later on it should be
-     * cloned. **/
-    virtual void onNotify(const GameEvent* event) = 0;
-};
+class Observer;
 
 class Observable {
   public:
+    /** The dtor will remove all observers **/
+    virtual ~Observable();
+    /** Will also update the observer's list of Observables **/
     void addObserver(Observer* observer,
                      const std::set<GameEvent::EventType>& eventTypes);
+    /** Will also update the observer's list of Observables **/
     void removeObserver(Observer* observer);
 
   protected:
@@ -26,6 +24,23 @@ class Observable {
 
   private:
     std::map<Observer*, std::set<GameEvent::EventType> > m_observers;
+};
+
+class Observer {
+  public:
+    virtual ~Observer();
+    /** The object event is pointing to is only guaranteed to exist at the time
+     * of notification. If access to the event is needed later on it should be
+     * cloned. **/
+    virtual void onNotify(const GameEvent* event) = 0;
+
+  private:
+    /** The Observable will add itself to this set in order to be able to clean
+     * up when an Observer or Observable is deleted. **/
+    std::set<Observable*> m_observing;
+    friend void Observable::addObserver(Observer*,
+                                        const std::set<GameEvent::EventType>&);
+    friend void Observable::removeObserver(Observer*);
 };
 
 }  // namespace VVipers
