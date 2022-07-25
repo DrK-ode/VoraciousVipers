840ec4ed487110e07907747ecbc14e2937300dfb
Kaj Jansson <mailtokaj@gmail.com>
2022-07-25 14:49:00 +0200

    Adds a wrapper for RNG

--- vvipers/VVMath.hpp
+++ vvipers/VVMath.hpp
@@ -2,6 +2,8 @@
 #define VVIPERS_VVMATH_HPP
 
 #include <cmath>
+#include <random>
+#include <limits>
 
 namespace VVipers {
 const double pi = 3.14159265358979323846;
@@ -27,6 +29,29 @@ inline double modPiRad(double rad) {
     return rad;
 }
 
+typedef std::ranlux48_base generatorEngine_t;
+class Random {
+  public:
+    static double getDouble() {
+        static std::uniform_real_distribution<double> distribution(0., 1.);
+        return distribution(m_generator);
+    }
+    // Both first and last inclusive
+    static int getInt(int first, int last) {
+        return getDouble(first, last+1);
+    }
+    // First inclusive, last exclusive
+    static double getDouble(double first, double last) {
+        return first + getDouble() * (last - first);
+    }
+
+  private:
+    static std::random_device m_randomDevice;
+    static generatorEngine_t m_generator;
+};
+inline std::random_device Random::m_randomDevice;
+inline generatorEngine_t Random::m_generator( Random::m_randomDevice() );
+
 }  // namespace VVipers
 
-#endif // VVIPERS_VVMATH_HPP
+#endif  // VVIPERS_VVMATH_HPP
