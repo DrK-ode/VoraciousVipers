32c36ef386bfc63bba05119c43f5b3ed2d3ab468
Kaj Jansson <mailtokaj@gmail.com>
2022-08-04 15:14:33 +0200

    Reads color from the configuration file

--- vvipers/Utilities/VVColor.hpp
+++ vvipers/Utilities/VVColor.hpp
@@ -2,17 +2,24 @@
 #define VVIPERS_UTILITIES_VVCOLOR_HPP
 
 #include <SFML/Graphics/Color.hpp>
-#include <string>
+#include <sstream>
+#include <vvipers/Utilities/debug.hpp>
 
-namespace VVipers
-{
+namespace VVipers {
 
-sf::Color stringToColor( const std::string& str){
-    return sf::Color::Red;
+sf::Color stringToColor(const std::string& str) {
+    if (str.size() != 7 or str.at(0) != '#')
+        throw std::runtime_error("Found invalid color string.");
+    unsigned int x;
+    std::stringstream ss;
+    ss << std::hex << str.substr(1);
+    ss >> x;
+    auto r = (x & 0xff0000) >> 16;
+    auto g = (x & 0x00ff00) >> 8;
+    auto b = (x & 0x0000ff) >> 0;
+    return sf::Color(r, g, b);
 }
 
-} // namespace VVipers
+}  // namespace VVipers
 
-
-
-#endif // VVIPERS_UTILITIES_VVCOLOR_HPP
+#endif  // VVIPERS_UTILITIES_VVCOLOR_HPP
